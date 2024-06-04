6cf818bf4a2500f73e433244517b514266aa3f97
Luana Martins <lmartinsarimitsu@gmail.com>
2024-06-04 14:24:07 -0300

    codigo com operadores de comparação

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 4;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute; 
+let tentativas  = 1;
 
-//se chute for igual ao número secreto
-if (chute == numeroSecreto){
-    alert(`Número secreto correto ${numeroSecreto}`);
-} else {
-    alert('Incorreto :(');
+//enquanto o chute não for igual ao número secreto 
+while (chute != numeroSecreto) {
+    chute = prompt ('Escolha um número de 1 a 10');
+    //se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Número secreto correto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++; 
+    }
 }
